0bb75ae3f2e77b6e748b95dc4a113e85a895eda6
m0k0s <carmenrob@gmail.com>
2018-04-03 21:07:14 -0400

    init

---
+++ twitter_config.py
@@ -0,0 +1,5 @@
+consumer_key = "SrvsaP1r613KXmc0eRgkPx0lt"
+consumer_secret = "1oWFPgk2C0zEysQf6yf2Holfhe00mA39nxYgFWjxdNl55UEYfG"
+access_token = "156819228-srweobwKQPijBqXqb4cWxjz81Xj6mV7EkvCnhxk7"
+access_token_secret = "aTe3FZjS69YCgaHQ3LXpJojyE9CnNSlQUhvhW54ufZ1Cl"
+

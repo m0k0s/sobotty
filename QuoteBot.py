8629a61ae7104385f2463721364e6ff50dcef74d
m0k0s <carmenrob@gmail.com>
2018-04-03 21:13:45 -0400

    init

---
+++ QuoteBot.py
@@ -0,0 +1,48 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Dependencies
+import tweepy
+import time
+from config import consumer_key, consumer_secret, access_token, access_token_secret
+
+
+# In[ ]:
+
+
+# Twitter API Keys
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
+
+
+# In[ ]:
+
+
+# Quotes to Tweet
+
+
+# In[ ]:
+
+
+# Create function for tweeting
+
+
+    # Twitter credentials
+
+
+    # Tweet the next quote
+
+
+    # Print success message
+
+
+# In[ ]:
+
+
+# Set timer to run every minute
+
